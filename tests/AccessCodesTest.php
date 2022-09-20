c688696d409e9879fe801f1c531278e32c286a54
Ravi Chodavarapu <rchodava@gmail.com>
2022-09-19 21:37:33 -0400

    Add delete access code request

--- tests/AccessCodesTest.php
+++ tests/AccessCodesTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 use Tests\Fixture;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientErrorResponseException as ClientErrorResponseException;
 
 final class AccessCodesTest extends TestCase
 {
@@ -25,5 +26,9 @@ final class AccessCodesTest extends TestCase
     $seam->access_codes->update(access_code_id: $access_code_id, code: "5678");
     $access_code = $seam->access_codes->get(access_code_id: $access_code_id);
     $this->assertTrue($access_code->code === "5678");
+
+    $seam->access_codes->delete(access_code_id: $access_code_id);
+    $access_code = $seam->access_codes->get(access_code_id: $access_code_id);
+    $this->assertEquals($access_code->status, "removing");
   }
 }
