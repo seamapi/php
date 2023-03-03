dcbf1d84f4f6f36593bd19815338d1f8f6539c39
Andrii Balitskyi <84702959+andrii-balitskyi@users.noreply.github.com>
2023-03-03 16:59:07 +0100

    Implement Multiple Code Creation (#40)
    * Add access_codes->create_multiple
    
    * Fix test

--- tests/AccessCodesTest.php
+++ tests/AccessCodesTest.php
@@ -13,22 +13,24 @@ final class AccessCodesTest extends TestCase
   {
     $seam = Fixture::getTestServer(true);
 
-    $device_id = $seam->devices->list()[0]->device_id;
+    $devices = $seam->devices->list();
+    $first_device_id = $devices[0]->device_id;
+    $second_device_id = $devices[1]->device_id;
 
-    $access_codes = $seam->access_codes->list(device_id: $device_id);
+    $access_codes = $seam->access_codes->list(device_id: $first_device_id);
     $this->assertEmpty($access_codes);
     $this->assertIsArray($access_codes);
 
-    $seam->access_codes->create(device_id: $device_id, code: "1235");
+    $seam->access_codes->create(device_id: $first_device_id, code: "1235");
 
-    $access_codes = $seam->access_codes->list(device_id: $device_id);
-    $this->assertIsArray($access_codes);
+    $access_codes = $seam->access_codes->list(device_id: $first_device_id);
+    $this->assertTrue(count($access_codes) > 0);
 
     $access_code_id = $access_codes[0]->access_code_id;
     $access_code = $seam->access_codes->get(access_code_id: $access_code_id);
     $this->assertTrue($access_code->access_code_id === $access_code_id);
 
-    $access_code = $seam->access_codes->get(device_id: $device_id, code: "1235");
+    $access_code = $seam->access_codes->get(device_id: $first_device_id, code: "1235");
     $this->assertTrue($access_code->code === "1235");
 
     $seam->access_codes->update(access_code_id: $access_code_id, code: "5679");
@@ -50,18 +52,21 @@ final class AccessCodesTest extends TestCase
     $end_date = new DateTime("+3 months");
 
     $seam->access_codes->create(
-      device_id: $device_id,
+      device_id: $first_device_id,
       code: "5679",
       starts_at: $start_date->format(DateTime::ATOM),
       ends_at: $end_date->format(DateTime::ATOM)
     );
 
-    $access_code = $seam->access_codes->list(device_id: $device_id)[0];
+    $access_code = $seam->access_codes->list(device_id: $first_device_id)[0];
 
     $formatted_starts_at = (new DateTime($access_code->starts_at))->format("Y-m-d");
     $formatted_ends_at = (new DateTime($access_code->ends_at))->format("Y-m-d");
 
     $this->assertTrue($formatted_starts_at === $start_date->format("Y-m-d"));
     $this->assertTrue($formatted_ends_at === $end_date->format("Y-m-d"));
+
+    $access_codes = $seam->access_codes->create_multiple(device_ids: [$first_device_id, $second_device_id]);
+    $this->assertTrue(count($access_codes) === 2);
   }
 }
