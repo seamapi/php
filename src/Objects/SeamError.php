370e884284864599aff851ca2378f1258fa73196
Andrii Balitskyi <84702959+andrii-balitskyi@users.noreply.github.com>
2023-03-23 18:21:37 +0100

    Fixes #45 (#46)

--- src/Objects/SeamError.php
+++ src/Objects/SeamError.php
@@ -11,15 +11,15 @@ class SeamError
     }
 
     return new self(
-      error_code: $json->error_code,
-      message: $json->message,
+      error_code: $json->error_code ?? null,
+      message: $json->message ?? null,
       created_at: $json->created_at ?? null,
     );
   }
 
   public function __construct(
-    public string $error_code,
-    public string $message,
+    public string|null $error_code,
+    public string|null $message,
     public string|null $created_at
   ) {
   }
