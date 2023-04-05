c4fb7f082338f10a59af2b4552dc22d4bc621044
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2023-04-05 16:19:09 +0530

    feat: partial

--- tests/Fixture.php
+++ tests/Fixture.php
@@ -8,7 +8,10 @@ final class Fixture
 {
   public static function getTestServer($load_devices = false)
   {
-    $seam = new SeamClient(getenv('SEAM_API_KEY'));
+    $seam = new SeamClient(
+      getenv('SEAM_API_KEY'), 
+      getenv('SEAM_URL')?? "https://connect.getseam.com"
+    );
     $seam->workspaces->reset_sandbox();
     if ($load_devices) {
       $seam->workspaces->_internal_load_schlage_factory();
