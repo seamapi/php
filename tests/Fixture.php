284e3fb4a22ab66cf556073c7e1f6a1f793a3c5c
andrii-balitskyi <10balian10@gmail.com>
2023-05-08 19:08:27 +0200

    Revert changes made for testing

--- tests/Fixture.php
+++ tests/Fixture.php
@@ -8,7 +8,7 @@ final class Fixture
 {
   public static function getTestServer($load_devices = false)
   {
-    $seam = new SeamClient('seam_test3CGc_4Desq33kSCEym8YNgxkW3CuB');
+    $seam = new SeamClient(getenv("SEAM_API_KEY"));
     $seam->workspaces->reset_sandbox();
     if ($load_devices) {
       $seam->workspaces->_internal_load_august_factory();
