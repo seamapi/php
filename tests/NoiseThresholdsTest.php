ea463fff292c27b05e8a80383412ddf041d34003
andrii-balitskyi <10balian10@gmail.com>
2023-05-26 15:52:24 +0200

    Uncomment and fix test

--- tests/NoiseThresholdsTest.php
+++ tests/NoiseThresholdsTest.php
@@ -9,47 +9,48 @@ final class NoiseThresholdsTest extends TestCase
 {
   public function testNoiseThresholds(): void
   {
-    // $seam = Fixture::getTestServer();
-    // $seam->workspaces->_internal_load_minut_factory();
+    $seam = Fixture::getTestServer();
+    $seam->workspaces->_internal_load_minut_factory();
 
-    // function getMinutDeviceNoiseThresholds($seam, $device_id)
-    // {
-    //   return $seam->noise_sensors->noise_thresholds->list([
-    //     'device_id' => $device_id
-    //   ]);
-    // }
+    function getMinutDeviceNoiseThresholds($seam, $device_id)
+    {
+      return $seam->noise_sensors->noise_thresholds->list(
+        device_id: $device_id
+      );
+    }
 
-    // $devices = $seam->devices->list();
-    // $device_id = $devices[0]->device_id;
+    $devices = $seam->devices->list();
+    $device_id = $devices[0]->device_id;
+    echo "id: $device_id\n";
 
-    // $noise_thresholds = getMinutDeviceNoiseThresholds($seam, $device_id);
-    // $this->assertTrue(count($noise_thresholds) === 2);
+    $noise_thresholds = getMinutDeviceNoiseThresholds($seam, $device_id);
+    $this->assertTrue(count($noise_thresholds) === 2);
 
-    // $quiet_hours_threshold = reset(array_filter($noise_thresholds, function ($nt) {
-    //   return $nt->name === 'builtin_quiet_hours';
-    // }));
+    $quiet_hours_threshold = array_filter($noise_thresholds, function ($nt) {
+      return $nt->name === 'builtin_quiet_hours';
+    });
 
-    // $devices = $seam->noise_sensors->noise_thresholds->delete(
-    //   device_id: $device_id,
-    //   noise_threshold_id: $quiet_hours_threshold["noise_threshold_id"],
-    // );
-    // $noise_thresholds = getMinutDeviceNoiseThresholds($seam, $device_id);
-    // $this->assertTrue(count($noise_thresholds) === 1);
+    $devices = $seam->noise_sensors->noise_thresholds->delete(
+      device_id: $device_id,
+      noise_threshold_id: reset($quiet_hours_threshold)->noise_threshold_id,
+    );
+    $noise_thresholds = getMinutDeviceNoiseThresholds($seam, $device_id);
+    $this->assertTrue(count($noise_thresholds) === 1);
 
-    // $noise_threshold = $seam->noise_sensors->noise_thresholds->create(
-    //   device_id: $device_id,
-    //   starts_daily_at: "20:00:00[America/Los_Angeles]",
-    //   ends_daily_at: "08:00:00[America/Los_Angeles]",
-    //   noise_threshold_decibels: 75,
-    // );
-    // $noise_thresholds = getMinutDeviceNoiseThresholds($seam, $device_id);
-    // $this->assertTrue(count($noise_thresholds) === 2);
+    $noise_threshold = $seam->noise_sensors->noise_thresholds->create(
+      device_id: $device_id,
+      starts_daily_at: "20:00:00[America/Los_Angeles]",
+      ends_daily_at: "08:00:00[America/Los_Angeles]",
+      noise_threshold_decibels: 75,
+    );
+    $noise_thresholds = getMinutDeviceNoiseThresholds($seam, $device_id);
+    $this->assertTrue(count($noise_thresholds) === 2);
 
-    // $updated_noise_threshold = $seam->noise_sensors->noise_thresholds->update(
-    //   device_id: $device_id,
-    //   noise_threshold_id: $noise_threshold->noise_threshold_id,
-    //   noise_threshold_decibels: 80,
-    // );
-    // $this->assertTrue($updated_noise_threshold->noise_threshold_decibels === 80);
+    $updated_noise_threshold = $seam->noise_sensors->noise_thresholds->update(
+      device_id: $device_id,
+      noise_threshold_id: $noise_threshold->noise_threshold_id,
+      noise_threshold_decibels: 80,
+    );
+    $this->assertTrue($updated_noise_threshold->noise_threshold_decibels == 80);
   }
 }
