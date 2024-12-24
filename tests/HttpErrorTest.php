e8ff2376fd9efc6b6b1962a34332fd7b65d70eaf
Andrii Balitskyi <10balian10@gmail.com>
2024-12-24 18:01:56 +0100

    Test non seam http errors

--- tests/HttpErrorTest.php
+++ tests/HttpErrorTest.php
@@ -54,4 +54,19 @@ final class HttpErrorTest extends TestCase
       $this->assertEquals('device_not_found', $e->errorCode);
     }
   }
+
+  public function testNonSeamError(): void
+  {
+    $seam = Fixture::getTestServer();
+
+    $seam = new \Seam\SeamClient("seam_apikey1_token", "https://nonexistent.example.com");
+
+    try {
+      $seam->devices->list();
+      $this->fail("Expected GuzzleHttp ConnectException");
+    } catch (\GuzzleHttp\Exception\ConnectException $e) {
+      $this->assertInstanceOf(\GuzzleHttp\Exception\ConnectException::class, $e);
+      $this->assertStringContainsString("Could not resolve host", $e->getMessage());
+    }
+  }
 }
